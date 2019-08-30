261e4cfdc883adb9512544c792ccfb20e8b0e4c3
touftoufa <touftoufa31@gmail.com>
2019-08-30 12:36:47 +0000

    ajoute Penda Sarr en TS2

---
+++ GestionEcole/helloworld.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main()
+{
+printf("hello world!  This is Geeksforgeeks.org " )
+return 0;
+}
---
+++ helloworld.c~
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main( )
+{
+printf("Hello World! This is Geeksforgeeks.org ")
+return 0;
+}
